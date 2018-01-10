295d44bf23b88d21cc72188eab80a397715de041
Mamithi <19lawrence93@gmail.com>
2018-01-10 18:56:01 +0300

    Adding yandex key

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1>Word Translator</h1>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+
   </div>
 </template>
 
 <script>
+  import TranslateForm from './components/TranslateForm'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TranslateForm
+  },
+  methods: {
+    translateText:function(text){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180110T155244Z.84768a0d100486a9.0a77e457a682d25f024157e7a256b9f9f0adb1e0&lang=sw&text='+text)
+        .then((response) => {
+          console.log(response)
+        })
+
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
